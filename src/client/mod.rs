370d50c34180ad5cab585b21b371fa7fd45343ed
vkropotko <vkropotko@riskfocus.com>
2019-03-28 16:12:15 +0200

    Add This -> https://github.com/clux/kubernetes-rust/commit/4e3589dd66149cc70c5f3adb42c14fffc52d84fc

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,8 +3,23 @@ use std::rc::Rc;
 use failure::Error;
 use http;
 use serde::de::DeserializeOwned;
+use serde_json;
 
 use super::config::Configuration;
+
+/// Error data returned by kube
+///
+/// Replacement data for reqwest::Response::error_for_status
+/// because it hardly ever includes good permission errors
+#[derive(Deserialize, Debug)]
+pub struct ApiError {
+    status: String,
+    #[serde(default)]
+    message: Option<String>,
+    #[serde(default)]
+    reason: Option<String>,
+    code: u16,
+}
 
 /// APIClient requires `config::Configuration` includes client to connect with kubernetes cluster.
 pub struct APIClient {
@@ -32,9 +47,71 @@ impl APIClient {
             other => {
                 return Err(Error::from(format_err!("Invalid method: {}", other)));
             }
-        }
-        .body(body);
+        }.body(body);
 
-        req.send()?.json().map_err(Error::from)
+        let mut res = req.send()?;
+        if !res.status().is_success() {
+            let text = res.text()?;
+            // Print better debug when things do fail
+            if let Ok(errdata) = serde_json::from_str::<ApiError>(&text) {
+                println!("Unsuccessful: {:?}", errdata);
+            } else {
+                // In case some parts of ApiError for some reason don't exist..
+                println!("Unsuccessful data: {}", text);
+            }
+            // Propagate errors properly via reqwest
+            let e = res.error_for_status().unwrap_err();
+            Err(e.into())
+        } else {
+            // Should be able to coerce result into T at this point
+            let text = res.text()?;
+            serde_json::from_str(&text).map_err(|e| {
+                println!("{}", text);
+                Error::from(e)
+            })
+        }
+    }
+
+    pub fn request_events<T>(&self, request: http::Request<Vec<u8>>) -> Result<Vec<T>, Error>
+    where
+        T: DeserializeOwned,
+    {
+        let (parts, body) = request.into_parts();
+        let uri_str = format!("{}{}", self.configuration.base_path, parts.uri);
+        let req = match parts.method {
+            http::Method::GET => self.configuration.client.get(&uri_str),
+            http::Method::POST => self.configuration.client.post(&uri_str),
+            http::Method::DELETE => self.configuration.client.delete(&uri_str),
+            http::Method::PUT => self.configuration.client.put(&uri_str),
+            other => {
+                return Err(Error::from(format_err!("Invalid method: {}", other)));
+            }
+        }.body(body);
+        let mut res = req.send()?;
+        if !res.status().is_success() {
+            let text = res.text()?;
+            // Print better debug when things do fail
+            if let Ok(errdata) = serde_json::from_str::<ApiError>(&text) {
+                println!("Unsuccessful: {:?}", errdata);
+            } else {
+                // In case some parts of ApiError for some reason don't exist..
+                println!("Unsuccessful data: {}", text);
+            }
+            // Propagate errors properly via reqwest
+            let e = res.error_for_status().unwrap_err();
+            Err(e.into())
+        } else {
+            // Should be able to coerce result into Vec<T> at this point
+            let mut xs: Vec<T> = vec![];
+            let text = res.text()?;
+            for l in text.lines() {
+                let r = serde_json::from_str(&l).map_err(|e| {
+                    println!("{}", l);
+                    Error::from(e)
+                })?;
+                xs.push(r);
+            }
+            Ok(xs)
+        }
     }
 }
