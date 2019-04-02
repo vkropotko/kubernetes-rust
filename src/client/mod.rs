31cfa0305b8673e0792a096b2038968144be7cbc
vkropotko <vkropotko@riskfocus.com>
2019-04-02 16:37:46 +0300

    Fix patch method

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -55,7 +55,10 @@ impl APIClient {
             http::Method::POST => self.configuration.client.post(&uri_str),
             http::Method::DELETE => self.configuration.client.delete(&uri_str),
             http::Method::PUT => self.configuration.client.put(&uri_str),
-            http::Method::PATCH => self.configuration.client.patch(&uri_str),
+            http::Method::PATCH => self.configuration
+                .client
+                .patch(&uri_str)
+                .header("Content-Type", "application/merge-patch+json"),
             other => {
                 return Err(Error::from(format_err!("Invalid method: {}", other)));
             }
